4bc5df484508358bb3b46c348bab80a2ff28008d
Riley Moore <rilemoore@gmail.com>
2019-10-14 17:56:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = []
   reverse_array = []
   array = sentence.split(" ")
-  array.each do |word|
+  array.collect do |word|
     reverse_array << (word.reverse)
   end
   return reverse_array.join(" ")
