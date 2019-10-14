a3477145a854417d247cc3df6587bb318ff5af24
Riley Moore <rilemoore@gmail.com>
2019-10-14 17:53:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   array = []
   reverse_array = []
   array = sentence.split(" ")
-  array.map do |word|
-    "#{word}".reverse
+  array.each do |word|
+    reverse_array << (word.reverse)
   end
   array
 end
